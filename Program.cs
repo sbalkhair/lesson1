2da7aad3466561ef85f0463be3e345164035eeef
saeed <s.balkhair@hotmail.com>
2020-10-10 12:03:23 +0300

    Initial check-in of module Lesson1

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+
+namespace Lesson1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            // print Hello word
+            //Console.WriteLine("Hello World");
+
+            //int x;
+            //int y = 10;
+            //var z=10;
+
+
+            //double x = 2.5;
+            //var y = 2.5;
+
+            //float x = 100.5f;
+
+            //decimal x = 1.5m;
+
+            //byte x = 1;
+            //bool x=true;
+            //char x = 'c';
+            //string y = "welcome in c#";
+
+
+            //var x = 10;
+            //var y = 20;
+            //Console.WriteLine("Your score :" + x);
+            //Console.WriteLine(string.Format("your score :{0} and {1}", x, y));
+            //Console.WriteLine($"your score is {x} and {y}");
+
+            //hello saeed
+            //welcome in our course      c#
+
+            //Console.WriteLine("hello saeed\nwelcome\nin our course\tc#");
+            //    Console.WriteLine(@"hello saeed
+            //welcome
+            //in out course           c#");
+
+
+            //saeed "programmer c#"
+            //Console.WriteLine("saeed \"programmer c#\"");
+            //Console.WriteLine($"min byte {byte.MinValue} max: {byte.MaxValue}");
+            //Console.WriteLine($"min int {int.MinValue} max: {int.MaxValue}");
+
+
+            //Console.Write("What is you name  ");
+            //var input = Console.ReadLine();
+
+
+            //Console.WriteLine($"Welcome {input}");
+
+
+
+
+            //Console.Write("Enter your name ");
+            //var inputName = Console.ReadLine();
+
+            //Console.Write("Enter your age ");
+            //var inputAge = Console.ReadLine();
+
+            //Console.WriteLine($"welcome {inputName} your age is {inputAge}");
+
+            int x = 10000;
+            byte y = (byte)x;
+            Console.WriteLine(y);
+
+
+        }
+    }
+}
---
+++ Women.cs
@@ -0,0 +1,7 @@
+﻿namespace Lesson1
+{
+    class Women
+    {
+
+    }
+}
